9edc444642d32cbaae015ba62c72d88f37fb0d4c
Nambong Ha <over3025@gmail.com>
2021-10-29 20:49:45 +0900

    #backtracking 14888 ing2...

--- backtracking/14888.py
+++ backtracking/14888.py
@@ -2,11 +2,17 @@ import sys
 N = int(sys.stdin.readline().strip())
 
 A = sys.stdin.readline().strip().split()
+for i in range(len(A)):
+    A[i] = int(A[i])
 
 sum, sub, mul, div = map(int, sys.stdin.readline().strip().split())
 
-print (N,A)
-print (sum, sub, mul, div)
+# N = 4
+# A = [2,3,4,5]
+# sum, sub, mul, div = 2,1,0,0
+
+# print (N,A)
+# print (sum, sub, mul, div)
 arr = []
 M = 0
 for _ in range(sum):
@@ -25,23 +31,55 @@ for _ in range(div):
     M +=1
     arr.append('/')
 
-print (arr)
+# print (arr)
 
-case = []
+# case = []
+# ans = []
 
-ans = []
-def bt():
-    if len(ans) == M:
-        case.append(ans)
-        print (f'{ans=}')
-        return
+import itertools
+
+case = list(itertools.permutations(arr,len(arr)))
+
+# print (f'{type(result[0])}')
+MAX = pow(10,9)*-1
+MIN = pow(10,9)
+
+# print (case)
+# case = [['-','/','+','+','*'],['+','+','/','-','*']]
+for c in case:
+    temp_list = deque(copy.deepopy())
+
+
+    res = 0
+
+    if c[0] == '+':
+        res = (A[0]+A[1])
+    elif c[0] == '-':
+        res = (A[0]-A[1])
+    elif c[0] =='*':
+        res = (A[0]*A[1])
+    else:        
+        res = A[0]//A[1]
+
+    #맨처음 A[1] 연산 A[2]
+    #두번째 위의 결과에 연산 A[3]
+    #세번째 위의 결과에 연산 A[4]
+    for i in range(1,len(c)): # 1,2,3 
+        # print (f'{res = } {A[i+1]=} {c[i]=}')
+        if c[i] == '+':
+            res +=A[i+1]
+        elif c[i] == '-':
+            res -=A[i+1]
+        elif c[i] =='*':
+            res *=A[i+1]
+        else:
+            if res < 0:
+                res = abs(res) // A[i+1]
+                res *=-1
+            else:
+                res = res // A[i+1]
     
-    for n in arr:
-        if n not in ans:
-            ans.append(n)
-            bt()
-            ans.pop
-        
-bt()
+    MIN = min(MIN, res)
+    MAX = max(MAX, res)
 
-print (case)
+print (MAX, MIN)
