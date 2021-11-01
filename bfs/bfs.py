d772618573096dd01a56a489ea8d72fd194f2523
Nambong Ha <over3025@gmail.com>
2021-11-01 17:34:54 +0900

    #bfs basic

--- bfs/bfs.py
+++ bfs/bfs.py
@@ -1,26 +1,24 @@
 from collections import deque
+import sys
 
 def bfs(graph, start, visited):
     queue = deque([start])
+    #현재 노드 방문 처리
     visited[start] = True
-    
+    #큐가 있는동안 반복
     while queue:
-        v=queue.popleft()
+        #큐에서 하나의 원소 출력
+        v = queue.popleft()
         print (v, end=' ')
+        #아직 방문하지 않은 인접한 원소 삽입
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
-                
 
-def bfs(V):
-    queue=[V]
-    visited[V] = 0
-    while queue:
-        V=queue.pop(0)
-        print (V, end=' ')
-        for i in range(1, N+1):
-            if visited[i]==1 and graph[V][i]==1:
-                queue.append(i)
-                visited[i]=0
-    
+input = sys.stdin.readline
+N = int(input())
+visited = [False] *(N+1)
+graph = []
+for _ in range(N):
+    graph.append(list(input().strip()))
