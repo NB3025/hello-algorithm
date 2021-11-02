811f63719369decabb913e7574af3f8ad02ff842
Nambong Ha <over3025@gmail.com>
2021-11-02 19:40:56 +0900

    programmers hash

---
+++ programmers/hash_2.py
@@ -0,0 +1,28 @@
+
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+
+    return answer
+
+
+def solution(phone_book):
+    phone_book = sorted(phone_book)
+
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+    
+phone_book = ["119", "97674223", "1195524421"]	
+
+print (solution(phone_book))
